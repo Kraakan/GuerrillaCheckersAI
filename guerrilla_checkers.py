9237dc7d9d3b57b9cb220f297ee06f1059173762
Kraakan <nkrogerus@gmail.com>
2025-03-21 15:33:55 +0200

    Squaring time penalty, because guerrilla seems to be suicidal

--- guerrilla_checkers.py
+++ guerrilla_checkers.py
@@ -395,7 +395,8 @@ class game():
         # Time penalty, might be useful if:
         # 1. It's small enough
         # 2. It applies correctly to both players
-        reward -= self.board[0]/64 # Guerrilla will have placed two stones already when this is first called
+        reward -= (self.board[0]/64)**2 # Guerrilla will have placed two stones already when this is first called
+        # g is suicidal! Trying squared time penalty
         reward = reward * self.small_reward_factor
         if player == 1: # Guerrilla
             reward *= -1
