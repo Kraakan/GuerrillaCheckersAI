8068772db00c5d7ad7ee5551b6a4d82f0705ab3a
Kraakan <nkrogerus@gmail.com>
2025-04-09 13:21:50 +0300

    Added code to generate more baselines + added random 1-checker games to agenda

--- test_baselines.py
+++ test_baselines.py
@@ -84,104 +84,71 @@ except FileNotFoundError:
         f.write("Avg. game length: " + str(avg_length))
         f.close()
 
-try:
-    random_vs_1_checker_baseline = open("data/random_g_vs_1_checker_baseline.txt", "r")
-except FileNotFoundError:
-    print("Running random guerrilla vs. random COIN with 1 starting checker:")
-    results = []
-    lengths = []
-    game = guerrilla_checkers.game(num_checkers=1)
-    for i in range(num_games):
-        print(str(i), "games run.", end="\r")
-        game.reset()
-        while not game.is_game_over():
-            state, player = game.get_current_state()
-            whose_turn_it_is = int(player)
-            valid_actions = game.get_valid_actions(player)
-            valid_actions_list = [k for k, v in valid_actions.items() if v == True]
-            selected_move = random.choice(valid_actions_list)
-            game.take_action(whose_turn_it_is, selected_move)
-        winner = game.get_game_result()
-        game_length = (66 - game.board[0])//2
-        results.append(winner)
-        lengths.append(game_length)
-    print("\n")
-    avg_length = statistics.mean(lengths)
-    g_win_rate = results.count(-1)/precentage_denominator
-    c_win_rate = results.count(1)/precentage_denominator
-    with open("data/random_g_vs_1_checker_baseline.txt", "w") as f:
-        f.write("Guerrilla wins: " + str(g_win_rate) + " %\n" + "COIN wins: " + str(c_win_rate) + " %\n")
-        f.write("Avg. game length: " + str(avg_length))
-        f.close()
-
-try:
-    random_vs_hardcoded_1_checker_baseline = open("data/random_g_vs_hardcoded_1_checker_baseline.txt", "r")
-except FileNotFoundError:
-    print("Running random guerrilla vs. hardcoded COIN with 1 starting checker:")
-    results = []
-    lengths = []
-    game = guerrilla_checkers.game(num_checkers=1)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    COIN = DQN.HardCoded(0, game, device)
-    for i in range(num_games):
-        game.reset()
-        while not game.is_game_over():
-            state, player = game.get_current_state()
-            whose_turn_it_is = int(player)
-            if whose_turn_it_is == 0:
-                action = COIN.select_action(state)
-                action = COIN.action_list[action.item()]
-                selected_move = tuple(action)
-            else:
+for c in range(1,7):
+    try:
+        existing_file = open("data/random_g_vs_" + str(c) + "_checker_baseline.txt", "r")
+        existing_file.close()
+    except FileNotFoundError:
+        print("Running random guerrilla vs. random COIN with " + str(c) + " starting checker:")
+        results = []
+        lengths = []
+        game = guerrilla_checkers.game(num_checkers=c)
+        for i in range(num_games):
+            print(str(i), "games run.", end="\r")
+            game.reset()
+            while not game.is_game_over():
+                state, player = game.get_current_state()
+                whose_turn_it_is = int(player)
                 valid_actions = game.get_valid_actions(player)
                 valid_actions_list = [k for k, v in valid_actions.items() if v == True]
                 selected_move = random.choice(valid_actions_list)
-            game.take_action(whose_turn_it_is, selected_move)
-        winner = game.get_game_result()
-        game_length = (66 - game.board[0])//2
-        results.append(winner)
-        lengths.append(game_length)
-    print("\n")
-    avg_length = statistics.mean(lengths)
-    g_win_rate = results.count(-1)/precentage_denominator
-    c_win_rate = results.count(1)/precentage_denominator
-    with open("data/random_g_vs_hardcoded_1_checker_baseline.txt", "w") as f:
-        f.write("Guerrilla wins: " + str(g_win_rate) + " %\n" + "COIN wins: " + str(c_win_rate) + " %\n")
-        f.write("Avg. game length: " + str(avg_length))
-        f.close()
+                game.take_action(whose_turn_it_is, selected_move)
+            winner = game.get_game_result()
+            game_length = (66 - game.board[0])//2
+            results.append(winner)
+            lengths.append(game_length)
+        print("\n")
+        avg_length = statistics.mean(lengths)
+        g_win_rate = results.count(-1)/precentage_denominator
+        c_win_rate = results.count(1)/precentage_denominator
+        with open("data/random_g_vs_" + str(c) + "_checker_baseline.txt", "w") as f:
+            f.write("Guerrilla wins: " + str(g_win_rate) + " %\n" + "COIN wins: " + str(c_win_rate) + " %\n")
+            f.write("Avg. game length: " + str(avg_length))
+            f.close()
 
-try:
-    random_vs_hardcoded_2_checker_baseline = open("data/random_g_vs_hardcoded_2_checker_baseline.txt", "r")
-except FileNotFoundError:
-    print("Running random guerrilla vs. hardcoded COIN with 2 starting checker:")
-    results = []
-    lengths = []
-    game = guerrilla_checkers.game(num_checkers=2)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    COIN = DQN.HardCoded(0, game, device)
-    for i in range(num_games):
-        game.reset()
-        while not game.is_game_over():
-            state, player = game.get_current_state()
-            whose_turn_it_is = int(player)
-            if whose_turn_it_is == 0:
-                action = COIN.select_action(state)
-                action = COIN.action_list[action.item()]
-                selected_move = tuple(action)
-            else:
-                valid_actions = game.get_valid_actions(player)
-                valid_actions_list = [k for k, v in valid_actions.items() if v == True]
-                selected_move = random.choice(valid_actions_list)
-            game.take_action(whose_turn_it_is, selected_move)
-        winner = game.get_game_result()
-        game_length = (66 - game.board[0])//2
-        results.append(winner)
-        lengths.append(game_length)
-    print("\n")
-    avg_length = statistics.mean(lengths)
-    g_win_rate = results.count(-1)/precentage_denominator
-    c_win_rate = results.count(1)/precentage_denominator
-    with open("data/random_g_vs_hardcoded_2_checker_baseline.txt", "w") as f:
-        f.write("Guerrilla wins: " + str(g_win_rate) + " %\n" + "COIN wins: " + str(c_win_rate) + " %\n")
-        f.write("Avg. game length: " + str(avg_length))
-        f.close()
+    try:
+        existing_file = open("data/random_g_vs_hardcoded_" + str(c) + "_checker_baseline.txt", "r")
+        existing_file.close()
+    except FileNotFoundError:
+        print("Running random guerrilla vs. hardcoded COIN with " + str(c) + " starting checker:")
+        results = []
+        lengths = []
+        game = guerrilla_checkers.game(num_checkers=c)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        COIN = DQN.HardCoded(0, game, device)
+        for i in range(num_games):
+            game.reset()
+            while not game.is_game_over():
+                state, player = game.get_current_state()
+                whose_turn_it_is = int(player)
+                if whose_turn_it_is == 0:
+                    action = COIN.select_action(state)
+                    action = COIN.action_list[action.item()]
+                    selected_move = tuple(action)
+                else:
+                    valid_actions = game.get_valid_actions(player)
+                    valid_actions_list = [k for k, v in valid_actions.items() if v == True]
+                    selected_move = random.choice(valid_actions_list)
+                game.take_action(whose_turn_it_is, selected_move)
+            winner = game.get_game_result()
+            game_length = (66 - game.board[0])//2
+            results.append(winner)
+            lengths.append(game_length)
+        print("\n")
+        avg_length = statistics.mean(lengths)
+        g_win_rate = results.count(-1)/precentage_denominator
+        c_win_rate = results.count(1)/precentage_denominator
+        with open("data/random_g_vs_hardcoded_" + str(c) + "_checker_baseline.txt", "w") as f:
+            f.write("Guerrilla wins: " + str(g_win_rate) + " %\n" + "COIN wins: " + str(c_win_rate) + " %\n")
+            f.write("Avg. game length: " + str(avg_length))
+            f.close()
